7fe698644859914a019328a54ed3748a040e0e66
MartinMatta <martinmatta355@gmail.com>
2021-07-14 21:37:37 +0200

    update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,6 +4,5 @@ import os
 import sys
 import time
 
-r = requests.get('https://10minutemail.net/')
-mail = re.findall(r'class="mailtext" value="(.*\@.+\.\w*)" />',r.content,re.I) 
-print( "\033[1;31mYour email:\033[0m \033[3;33m"+email[0]+"\033[0m")
+url = 'https://10minutemail.net/'
+
