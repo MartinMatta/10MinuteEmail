d845e4074c92bc21c3aeef919495386f01072b49
MartinMatta <martinmatta355@gmail.com>
2021-07-15 20:41:53 +0200

    read email from web

--- __main__.py
+++ __main__.py
@@ -1,8 +1,17 @@
 import requests
-import re 
-import os
-import sys
-import time
+from bs4 import BeautifulSoup
+
 
 url = 'https://10minutemail.net/'
 
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+table = soup.find('mailbox-table')
+
+table_row = table.find_all('tr')
+
+print(table_row)
+
+print(soup.find(id='fe_text').get("value"))
