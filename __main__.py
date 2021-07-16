c6de0bb2003d57137f106a7ac06751559325de86
MartinMatta <martinmatta355@gmail.com>
2021-07-16 21:16:44 +0200

    add read email table

--- __main__.py
+++ __main__.py
@@ -8,10 +8,13 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
-table = soup.find('mailbox-table')
+table = soup.find('table')
 
-table_row = table.find_all('tr')
+table_rows = table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
 
-print(table_row)
 
 print(soup.find(id='fe_text').get("value"))
